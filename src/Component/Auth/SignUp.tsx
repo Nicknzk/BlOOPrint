29d49449d1ddb51806531e839ac692d098845176
Nicholas Ng Ze Kai <92707766Nzk@gmail.com>
2023-06-05 18:58:08 +0800

    Set required for password & email Set password requirement (min 8 char, max 20 char, must contain 1 number and 1 letter) Set alert system that will only appear if user password creation is wrong.

--- src/Component/Auth/SignUp.tsx
+++ src/Component/Auth/SignUp.tsx
@@ -1,5 +1,4 @@
-import { Form, Button, Card } from "react-bootstrap";
-//import { Button, Typography } from "@mui/material";
+import { Form, Button, Card, Alert } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { useState } from "react";
 import {
@@ -8,133 +7,96 @@ import {
 } from "firebase/auth";
 import auth from "../../firebase";
 import "../../CSS-Folder/Auth.css";
-//import AuthDetails from "./AuthDetails";
 
 export default function SignUp() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [alertShow, setAlertShow] = useState("");
+  let messages: string[] = [];
+
+  function passTest() {
+    setAlertShow("");
+    if (password.length < 8) {
+      messages.push("Password must be minimum 8 characters");
+    }
+
+    if (password.length > 20) {
+      messages.push("Password cannot exceed 20 characters");
+    }
+
+    if (password.search(/[a-z]/i) < 0) {
+      messages.push("Password must contain atleast one letter");
+    }
+
+    if (password.search(/[0-9]/) < 0) {
+      messages.push("Password must contain atleast one number");
+    }
+  }
 
   const signUp = (event: any) => {
+    passTest(); //check if password is in the correct format
     event.preventDefault();
-    createUserWithEmailAndPassword(auth, email, password)
-      .then((userCredential) => {
-        console.log(userCredential);
-        sendEmailVerification(userCredential.user)
-          .then(() => {
-            console.log("Verification email sent.");
-          })
-          .catch((error) => {
-            console.log("Error sending verification email:", error);
-          });
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+    if (messages.length === 0) {
+      createUserWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+          console.log(userCredential);
+          sendEmailVerification(userCredential.user)
+            .then(() => {
+              console.log("Verification email sent.");
+            })
+            .catch((error) => {
+              console.log("Error sending verification email:", error);
+            });
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    } else {
+      setAlertShow(messages.join("\n - \n"));
+    }
   };
-
-  return (
-    <div className="sign-in-container log-in-box">
-      <Card>
-        <Card.Body>
-          <h2 className="text-center mb-4">Create Account</h2>
-          <Form onSubmit={signUp}>
-            <Form.Group className="text-center">
-              <Form.Label>Email: </Form.Label>
-              <input
-                type="email"
-                placeholder="Enter your email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-              ></input>
-            </Form.Group>
-            <Form.Group className="text-center">
-              <Form.Label>Password: </Form.Label>
-              <input
-                type="password"
-                placeholder="Enter your password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-              ></input>
-            </Form.Group>
-            <Button className="w-100" type="submit">
-              Sign Up
-            </Button>
-          </Form>
-        </Card.Body>
-      </Card>
-      <div className="w-100 text-center mt-2">
-        already have an account?
-        <nav>
-          <Link to={"/SignIn"}>Sign In</Link>
-        </nav>
-      </div>
-    </div>
-  );
-}
-/* decoration
-<Card>
-        <Card.Body>
-          <h2 className="text-center mb-4">Sign Up</h2>
-          <Form>
-            <Form.Group id="email">
-              <Form.Label>Email</Form.Label>
-              <Form.Control type="email" ref={emailRef} required />
-            </Form.Group>
-            <Form.Group id="password">
-              <Form.Label>Password</Form.Label>
-              <Form.Control type="password" ref={passwordRef} required />
-            </Form.Group>
-            <Form.Group id="password-confirm">
-              <Form.Label>Password Confirmation</Form.Label>
-              <Form.Control type="password" ref={passwordConfirmRef} required />
-            </Form.Group>
-            <Button className="w-100" type="submit">
-              Sign Up
-            </Button>
-          </Form>
-        </Card.Body>
-      </Card>
-      <div className="w-100 text-center mt-2">
-        already have an account? Log in
-      </div>
-*/
-
-/*import { Typography } from "@mui/material";
-import { styled } from "@mui/material/styles";
-import Icon from "./Icon.tsx";
-import { Form, Button, Card } from 'react-bootstrap'
-
-
-export default function LogInSignUp() {
-  const handleSubmit = (event: any) => {
-    event.preventDefault();
-  };
-
-  const MyCustom = styled(Typography)({
-    fontSize: "3rem",
-  });
 
   return (
     <>
-      <MyCustom>Create Account</MyCustom>
-      <Typography variant="h6">
-        Create an account to view and mange your mindmaps
-      </Typography>
-      <form method="get" onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="email">Email:</label>
-          <input type="email" id="email" />
-          <label htmlFor="text">Username:</label>
-          <input type="text" id="text" />
-          <label htmlFor="password">Password:</label>
-          <input type="password" id="password" />
-          <label htmlFor="confirmPassword">Confirm Password:</label>
-          <input type="password" id="confirmPassword" />
+      <div className="sign-in-container log-in-box">
+        <Card>
+          <Card.Body>
+            <h2 className="text-center mb-4">Create Account</h2>
+            <Form onSubmit={signUp}>
+              <Form.Group className="text-center">
+                <Form.Label>Email: </Form.Label>
+                <input
+                  type="email"
+                  placeholder="Enter your email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  required
+                ></input>
+              </Form.Group>
+              <Form.Group className="text-center">
+                <Form.Label>Password: </Form.Label>
+                <input
+                  type="password"
+                  placeholder="Enter your password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  required
+                ></input>
+              </Form.Group>
+              <Button className="w-100" type="submit">
+                Sign Up
+              </Button>
+            </Form>
+          </Card.Body>
+        </Card>
+        <div className="w-100 text-center mt-2">
+          already have an account?
+          <nav>
+            <Link to={"/SignIn"}>Sign In</Link>
+          </nav>
         </div>
-        <button type="submit">Submit</button>
-      </form>
-      <Icon />
+      </div>
+      <div>{alertShow.length > 0 && <Alert>{alertShow}</Alert>}</div>
     </>
   );
 }
-*/
