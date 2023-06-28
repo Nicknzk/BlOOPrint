7320960d8049b0ef238e89ae22b0eb9e48f46e6a
Nicholas Ng Ze Kai <92707766Nzk@gmail.com>
2023-06-28 13:42:47 +0800

    Added direct navigation from page to homePage upon login

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import AuthDetails from "./Component/Auth/AuthDetails";
 import PageRouting from "./Component/NavigationFolder/PageRouting";
+import { useNavigation } from "./Component/NavigationFolder/UseNavigation";
 
 export default function App() {
   const [authUser, setAuthUser] = useState(false);
+  const navigation = useNavigation();
 
   const handleAuthStatusChange = (trueOrFalse: any) => {
     setAuthUser(trueOrFalse);
   };
+
+  useEffect(() => {
+    if (authUser) {
+      navigation("/HomePage"); // Navigate to the homepage
+    }
+  }, [authUser]);
 
   return (
     <>
