854e46c2ab40a2bf4c58df521526f1378287ce45
Nicholas Ng Ze Kai <92707766Nzk@gmail.com>
2023-07-12 16:57:26 +0800

    adjusted error from coverage

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
-import { defineConfig } from 'vitest/config'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 
 export default defineConfig({
   plugins: [react()],
-  test: {
+  /*test: {
     coverage: {
       reporter: ["text", "html"],
     },
-  },
+  },*/
 })
+
