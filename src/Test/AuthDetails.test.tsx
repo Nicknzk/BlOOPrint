8b91cc0c0ec0aa7f848751cf5452aa270979dd55
Nicholas Ng Ze Kai <92707766Nzk@gmail.com>
2023-07-12 16:48:31 +0800

    Added in Vitest, + configurations for HTML results after test

---
+++ src/Test/AuthDetails.test.tsx
@@ -0,0 +1,8 @@
+import AuthDetails from "../Component/Auth/AuthDetails";
+import { describe, expect, it } from "vitest";
+
+describe("#AuthDetails", () => {
+  it("Authentication runs smoothly", () => {
+    expect(AuthDetails()).toBe(0);
+  });
+});
